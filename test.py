3d872590beaf017b2c52969abd56d47022c96538
perrinthibaud <thibaud.perrin@outlook.com>
2019-12-19 17:51:12 +0100

    update

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import argparse
 import sys
 
@@ -5,6 +6,9 @@ import gym
 from gym import wrappers, logger
 import matplotlib.pyplot as plt
 import torch
+import torch.nn.functional as F
+import numpy as np
+import random
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -14,12 +18,12 @@ class RandomAgent(object):
     def act(self, observation, reward, done):
         return self.action_space.sample()
 
-class ListFifo():
+class Memory():
     def __init__(self, size):
         self.listFifo = []
         self.maxSize = size
         self.counter = 0
-    def add(self, value):
+    def remember(self, value):
         if len(self.listFifo) < self.maxSize and self.counter < self.maxSize:
             self.listFifo.append(value)
         else :
@@ -27,6 +31,23 @@ class ListFifo():
         self.counter+=1
     def show(self):
         print(self.listFifo)
+    def getAll(self):
+        return self.listFifo
+
+class MultipleLayer(torch.nn.Module):
+    def __init__(self, D_in, H, D_out , nbcouche):
+        super(MultipleLayer, self).__init__()
+        self.n_couche = nbcouche
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.w = [torch.nn.Linear(H,H) for i in range(nbcouche)]
+        self.linear2 = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        y_pred = F.sigmoid(self.linear1(x))
+        for n in range(self.n_couche-1):
+            y_pred = F.sigmoid(self.w[n](y_pred))
+        y_pred = self.linear2(y_pred)
+        return y_pred
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
@@ -53,21 +74,48 @@ if __name__ == '__main__':
     episode_count = 100
     reward = 1
     done = False
-    listFifo = ListFifo(100000)
-    # for i in range(12):
-    #     tensorAdd = torch.tensor([[[i],[i]]],dtype=float)
-    #     # tensorAdd = tensorAdd.permute(1,0)
-    #     listFifo.add(tensorAdd)
+    memory = Memory(100000)
 
+    model = MultipleLayer(4, 100, 2, 2)
+
+    # Construct our loss function and an Optimizer. The call to model.parameters()
+    # in the SGD constructor will contain the learnable parameters of the two
+    # nn.Linear modules which are members of the model.
+    loss_fn = torch.nn.MSELoss(reduction='sum')
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    for t in range(nombre_iterations):
+        for index, (image, label) in enumerate(train_loader) :
+            counter+=1
+            if index % 10 == 0: # evite la surcharge de message du notebook afin de ne pas avoir un message d'erreur
+                progress = (counter * 100 / (nombre_iterations*len(train_loader)))
+                print("Progress {:0.2f}%".format(progress), end="\r")
+            
+            # w_2c = w_2c.permute(1,0)
+            x = image
+            y_pred = model(x)
+
+            # Compute and print loss
+            loss = loss_fn(y_pred, label)
+
+            # Zero gradients, perform a backward pass, and update the weights.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step() 
+
+ # target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
 
     for i in range(episode_count):
         somme = 0
-        ob = env.reset()
+        etat_suivant = env.reset()
+        etat = etat_suivant
         while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            tensorAdd = [[[ob,[reward],ob,[1. if done else 0. ]]]]
-            listFifo.add(tensorAdd)
+            action = agent.act(etat_suivant, reward, done)
+            etat_suivant, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            tensorAdd = [(etat,action,etat_suivant, reward,1. if done else 0. )]
+            etat = etat_suivant
+            memory.remember(tensorAdd)
             somme+= reward
             if done:
                 break
@@ -76,13 +124,21 @@ if __name__ == '__main__':
             # Video is not recorded every episode, see capped_cubic_video_schedule for details.
         listSomme.append(somme)
     # Close the env and write monitor result info to disk
-    listFifo.show()
-    
+    # memory.show()
+    minibatch = random.sample(memory.getAll(), 50)
 
     x = np.arange(episode_count) 
     y = np.array(listSomme)
     plt.plot(x, y, "-ob", markersize=2, label="nom de la courbe")
     plt.show()
 
-
     env.close()
+
+
+
+
+
+
+
+
+
