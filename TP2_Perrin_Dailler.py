81f91f9cb6905fa7075449948ad2e5991ae1e074
perrinthibaud <thibaud.perrin@outlook.com>
2020-01-02 15:04:24 +0100

    add model

--- TP2_Perrin_Dailler.py
+++ TP2_Perrin_Dailler.py
@@ -8,14 +8,12 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.functional as F
 import numpy as np
 import random
 
 
-# from https://github.com/keon/deep-q-learning/blob/master/dqn_batch.py
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
+
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -26,43 +24,28 @@ class RandomAgent(object):
         =======
             size (int): size of the memory
             memory (array()): memory of the agent
-            batch_size (int): size of the part of memory which is selected
-            state_size (int): dimension of each state
-            action_size (int): dimension of each action
-            seed (int): random seed
+            batch_size (int): size of the part of memory which is selected (N)
+            state_size (int): dimension of each state (D_in)
+            action_size (int): dimension of each action (D_out)
         """
         
         self.action_space = action_space
         self.size = 100000 # Memory size
         self.memory = []
         self.batch_size = 32
+        self.state_size = 4 
+        self.action_size = 2
+        self.learning_rate = 1e-3
+        self.model = MultipleLayer(self.state_size, 100, self.action_size, 2)
+        self.Tau = 0.5
 
-        self.state_size = 4
-        self.action_size = 1
-        # self.seed = random.seed(seed)
+        self.loss_fn = torch.nn.MSELoss(reduction='sum')
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
 
-        #Q- Network
-        # self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        # self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
-        self.model = self._build_model()
-
-    def _build_model(self):
-        # Neural Net for Deep-Q learning Model
-        model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
-        return model
-
+    # action 1 = droite action 0 = gauche
     def act(self, observation, reward, done):
+        #P(s,ak) = ( e( Q(s,ak) / self.Tau ) / torch.sum( e( Q(s,ai) / self.Tau ) ) )
         return self.action_space.sample()
 
     def remember(self, value):
@@ -76,74 +59,41 @@ class RandomAgent(object):
     def getMemory(self):
         return self.memory
 
+    def learn(self, x, y):
+        print(x)
+        print(torch.tensor(x).float())
+        y_pred = self.model(torch.tensor(x).float())
+
+        # Compute and print loss
+        loss = self.loss_fn(y_pred, y.float())
+
+        # Zero gradients, perform a backward pass, and update the weights.
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+
     def retry(self, batch_size):
         minibatch = random.sample(self.memory, self.batch_size)
-        for etat, action, etat_suivant, reward, done in minibatch:
-            target = reward
-            if not done:
-                target = (reward + self.gamma *
-                          np.amax(self.model.predict(next_state)[0]))
-            target_f = self.model.predict(state)
-            target_f[0][action] = target 
-            # Filtering out states and targets for training
-            states.append(state[0])
-            targets_f.append(target_f[0])
-        history = self.model.fit(np.array(states), np.array(targets_f), epochs=1, verbose=0)
-        # Keeping track of loss
-        loss = history.history['loss'][0]
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-        return loss
-
-
-# j'ai pris le code ici il faut essayer de comprendre XD:
-# https://medium.com/@unnatsingh/deep-q-network-with-pytorch-d1ca6f40bfda
-class QNetwork(nn.Module):
-    """ Actor (Policy) Model."""
-    def __init__(self, state_size, action_size, seed, fc1_unit=64,
-                 fc2_unit = 64):
-        """
-        Initialize parameters and build model.
-        Params
-        =======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_unit (int): Number of nodes in first hidden layer
-            fc2_unit (int): Number of nodes in second hidden layer
-        """
-        super(QNetwork,self).__init__() ## calls __init__ method of nn.Module class
-        self.seed = torch.manmual_seed(seed)
-        self.fc1= nn.Linear(state_size,fc1_unit)
-        seed.fc2 = nn.Linear(fc1_unit,fc2_unit)
-        seed.fc3 = nn.Linear(fc2_unit,action_size)
-        
-    def forward(self,x):
-        # x = state
-        """
-        Build a network that maps state -> action values.
-        """
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
 
 
 
+class MultipleLayer(torch.nn.Module):
+    def __init__(self, D_in, H, D_out, nbcouche):
+        super(MultipleLayer, self).__init__()
+        self.n_couche = nbcouche
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.w = [torch.nn.Linear(H,H) for i in range(nbcouche)]
+        self.linear2 = torch.nn.Linear(H, D_out)
 
-# class MultipleLayer(torch.nn.Module):
-#     def __init__(self, D_in, H, D_out , nbcouche):
-#         super(MultipleLayer, self).__init__()
-#         self.n_couche = nbcouche
-#         self.linear1 = torch.nn.Linear(D_in, H)
-#         self.w = [torch.nn.Linear(H,H) for i in range(nbcouche)]
-#         self.linear2 = torch.nn.Linear(H, D_out)
+    def forward(self, x):
+        y_pred = F.sigmoid(self.linear1(x))
+        for n in range(self.n_couche-1):
+            y_pred = F.sigmoid(self.w[n](y_pred))
+        y_pred = self.linear2(y_pred)
+        return y_pred
 
-#     def forward(self, x):
-#         y_pred = F.sigmoid(self.linear1(x))
-#         for n in range(self.n_couche-1):
-#             y_pred = F.sigmoid(self.w[n](y_pred))
-#         y_pred = self.linear2(y_pred)
-#         return y_pred
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
@@ -170,10 +120,7 @@ if __name__ == '__main__':
     episode_count = 100
     reward = 1
     done = False
-    # memory = Memory(100000)
     batch_size = 32
-
-    # model = MultipleLayer(4, 100, 2, 2)
 
     # Construct our loss function and an Optimizer. The call to model.parameters()
     # in the SGD constructor will contain the learnable parameters of the two
@@ -213,8 +160,10 @@ if __name__ == '__main__':
             reward = reward if not done else -10
             tensorAdd = (etat, action, etat_suivant, reward, done)
             etat = etat_suivant
+            agent.learn(etat, torch.tensor([1.,0.], dtype=float) if action == 0 else torch.tensor([0,1], dtype=float))
             agent.remember(tensorAdd)
             somme+= reward
+            # agent.learn(etat, reward)
             if done:
                 break
             if len(agent.memory) > batch_size:
