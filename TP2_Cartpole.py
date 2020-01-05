a5b08ccc3291c7b652c6d2ab6908cd41526a14a5
perrinthibaud <thibaud.perrin@outlook.com>
2020-01-05 23:05:02 +0100

    clean cartpole

--- TP2_Cartpole.py
+++ TP2_Cartpole.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import argparse
-import sysm
+import sys
 import gym
 from gym import wrappers, logger
 import matplotlib.pyplot as plt
@@ -90,7 +90,7 @@ class RandomAgent(object):
             else :
                 JO = pow(qOsa - reward, 2)
             loss = self.loss_fn(qOsa, JO)
-            # Zero gradients, perform a backward pass, and update the weights.
+
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
@@ -98,10 +98,8 @@ class RandomAgent(object):
             if (self.learn_state % 10000 == 0):
                 print("learn_state : ", self.learn_state)
                 self.upadteModel()
-                # self.model_duplicata.w = self.model.w
 
             self.learn_state +=1
-
 
 
 class MultipleLayer(torch.nn.Module):
@@ -120,24 +118,16 @@ class MultipleLayer(torch.nn.Module):
         return y_pred
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('env_id', nargs='?', default='CartPole-v1', help='Select the environment to run')
     args = parser.parse_args()
 
-    # You can set the level to logger.DEBUG or logger.WARN if you
-    # want to change the amount of output.
     logger.set_level(logger.INFO)
 
-    # print(logger)
 
     env = gym.make(args.env_id)
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
     outdir = '/tmp/random-agent-results'
     env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
@@ -145,7 +135,7 @@ if __name__ == '__main__':
     listSomme = []
     episode_count = 260
     reward = 1
-
+    max_reward = 500
 
     etat_space = env.observation_space.shape[0]
     action_space = env.action_space.n
@@ -157,33 +147,26 @@ if __name__ == '__main__':
         done = False
         
         while True:
-            #  aenv.render()
+            #  env.render()
             action = agent.act(etat, reward, done)
             etat_suivant, reward, done, _ = env.step(action)
             reward = reward if not done else -10
             tensorAdd = (etat, action, etat_suivant, reward, done)
-            # agent.learn(etat, torch.tensor([1.,0.], dtype=float) if action == 0 else torch.tensor([0,1], dtype=float))
+
             agent.remember(tensorAdd)
             etat = etat_suivant
 
             somme += reward
-            # agent.learn(etat, reward)
             if done:
-                # agent.upadteModel()
+                agent.upadteModel()
+                break
+            if somme > max_reward:
                 break
 
             if len(agent.memory) > agent.batch_size:
-                # loss = agent.retry(batch_size)
                 agent.retry(agent.batch_size)
-            
 
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
         listSomme.append(somme)
-    # Close the env and write monitor result info to disk
-    # agent.showMemory()
-    # minibatch = random.sample(agent.getMemory(), batch_size)
 
     x = np.arange(episode_count) 
     y = np.array(listSomme)
